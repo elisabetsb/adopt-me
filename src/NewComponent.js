28dd7bae6302f993cdacfc34c0a2236bc33de722
elisabetsb <elisanbri.87@gmail.com>
2020-08-19 20:51:16 +0100

    Adding a Single component

---
+++ src/NewComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class NewComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hola que tal</h1>
+      </div>
+    );
+  }
+}
+
+export default NewComponent;
