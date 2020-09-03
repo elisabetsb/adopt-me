d437df2d930a9e236742d5d095f8dd997386ad2f
elisabetsb <elisanbri.87@gmail.com>
2020-09-03 11:08:30 +0100

    move pet details to NewComponent

--- src/NewComponent.js
+++ src/NewComponent.js
@@ -1,10 +1,24 @@
 import React from "react";
 
 class NewComponent extends React.Component {
+  state = {};
+
+  static getDerivedStateFromProps({
+    name,
+    animal,
+    breed,
+    location,
+    description
+  }) {
+    return { name, animal, breed, location, description };
+  }
   render() {
+    const { name, animal, breed, location, description } = this.state;
     return (
       <div>
-        <h1>{this.props.text}</h1>
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+        <p>{description}</p>
       </div>
     );
   }
